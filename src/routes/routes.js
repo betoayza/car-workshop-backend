294d2b77ef7c85c4b06951970342deb3428d5e14
Alberto <aaayza@gmail.com>
2022-07-01 20:26:46 -0300

    Testing Heroku

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,10 @@ import ServiceModel from "../models/serviceModel.js";
 
 const router = Router();
 
+router.get("/", (req, res=>{
+  console.log("asd");
+}));
+
 //router for  list with all cars with more 3 years old and just 1 service done
 router.get("/cars/search/lists/carlist1", async (req, res) => {
   try {
