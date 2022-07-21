a144f25ea838cfa39267468bf91267958019b168
Alberto <aaayza@gmail.com>
2022-07-20 21:01:58 -0300

    Testing Heroku

--- src/index.js
+++ src/index.js
@@ -1,41 +1,39 @@
-import express from 'express';
-import routes from './routes/routes.js';
-import cors from 'cors';
-import morgan from 'morgan';
+import express from "express";
+import routes from "./routes/routes.js";
+import cors from "cors";
+import morgan from "morgan";
 
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(routes);
 
-app.use(cors());
-app.options('*', cors())
+app.use(cors({ origin: true }));
+// app.options('*', cors())
 
 //Use the logger package we have imported to get the log details of our application if needed.
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            name: 'Server running on port 5000 :)'            
-        }
-    });
-
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      name: "Server running on port 5000 :)",
+    },
+  });
 });
 
 // app.get("/favicon.ico", (req, res) => res.status(204));
 
-
 app.listen(PORT, () => {
-    console.log('Servidor corriendo en puerto 5000...');    
+  console.log("Servidor corriendo en puerto 5000...");
 });
-
