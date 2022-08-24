6631a72ac86b7f52d93b6166327db9b9600b3183
Alberto <aaayza@gmail.com>
2022-08-24 17:01:24 -0300

    Search Service updated

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -412,9 +412,7 @@ router.get("/services/search", async (req, res) => {
     console.log(req.query);
     const { code } = req.query;
     console.log(code);
-    let doc = await ServiceModel.findOne({
-      $and: [{ code }, { status: "Active" }],
-    }).exec();
+    let doc = await ServiceModel.findOne({ code }).exec();
     if (doc) {
       console.log(doc);
       res.json(doc);
